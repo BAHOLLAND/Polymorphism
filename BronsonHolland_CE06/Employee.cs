46e49abcf8e1c82c963f44521dbd2337656a3b67
Bronson Holland <baholland@student.fullsail.edu>
2020-05-30 09:29:29 -0700

    initial commit

---
+++ BronsonHolland_CE06/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BronsonHolland_CE06
+{
+    public class Employee
+    {
+        public string name;
+        public string address;
+
+        public Employee()
+        {
+        }
+    }
+}
---
+++ BronsonHolland_CE06/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BronsonHolland_CE06
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write(
+                    "1. Add Employee\n" +
+                    "2. Remove Employee\n" +
+                    "3. Display Payroll\n" +
+                    "4. Exit\n" +
+                    "Please select one of the options: ");
+                string input = Console.ReadLine().ToLower();
+
+                switch (input)
+                {
+                    case "1":
+                    case "add employee":
+                        {
+                            Console.Clear();
+                        }
+                        break;
+                    case "2":
+                    case "remove employee":
+                        {
+                            Console.Clear();
+                        }
+                        break;
+                    case "3":
+                    case "display payroll":
+                        {
+                            Console.Clear();
+                        }
+                        break;
+                    case "4":
+                    case "exit":
+                        {
+
+                            return;
+                        }
+                }
+            }
+        }
+    }
+}
